501d5f27cede33b49c0f9dde90f779d86197413e
JampoolG <Jampoolgarcia@gmail.com>
2022-12-16 23:36:19 -0300

    start proyect electron

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+
+
+let appWin;
+
+createWindow = () => {
+  appWin = new BrowserWindow({
+    width: 800,
+    height: 600,
+    title: "first app electron.",
+    resizable: false,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  });
+
+  appWin.loadURL(`file://${__dirname}/dist/index.html`);
+
+  appWin.setMenu(null);
+
+  appWin.webContents.openDevTools();
+
+  appWin.on("closed", ()=>{
+    appWin = null;
+  });
+
+}
+
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () =>{
+  if(process.platform !== "darwin") {
+    app.quit();
+  }
+});
