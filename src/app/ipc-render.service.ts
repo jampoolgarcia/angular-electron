deebc5791957e42a204a26432e5422854a1b5fa3
JampoolG <Jampoolgarcia@gmail.com>
2022-12-17 09:50:56 -0300

    add metho on, once, send, removeAllListener in ipcRenderService

--- src/app/ipc-render.service.ts
+++ src/app/ipc-render.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 
+// importa la clase ipcRenderer desde electron
 import { IpcRenderer } from 'electron';
 
 @Injectable({
@@ -7,17 +8,44 @@ import { IpcRenderer } from 'electron';
 })
 export class IpcRenderService {
 
+  // alamacena el conector ipcRender
   private ipc!: IpcRenderer;
 
   constructor() { 
+    // comprueba que window.require esta disponible;
     if(window.require){
+      // si es asi, la app esta funcianando con electron
       try {
+        // ahora podemos cargar el ipcRenderer
         this.ipc = window.require("electron").ipcRenderer;
       } catch (e) {
+        // por si sucede algun error lo imprimimos
         throw e;
       }
     }else{
+      // msg si no se pudo cargar
       console.warn("Electron ipcRender was not loaded");
     }
+  }
+
+
+  public on(channel: string, listener: any): void {
+    if(!this.ipc) return;
+    this.ipc.on(channel, listener);
+  }
+
+  public once(channel: string, listener: any): void {
+    if(!this.ipc) return;
+    this.ipc.once(channel, listener);
+  }
+
+  public send(channel: string, ...arg: any[]): void {
+    if(!this.ipc) return;
+    this.ipc.send(channel, arg);
+  }
+
+  public removeAllListener(channel: string):void{
+    if(!this.ipc) return;
+    this.ipc.removeAllListeners(channel);
   }
 }
