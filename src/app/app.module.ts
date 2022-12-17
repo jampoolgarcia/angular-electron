9343accb230bd3a75b80775db229621f2bc0586e
JampoolG <Jampoolgarcia@gmail.com>
2022-12-17 12:01:54 -0300

    create menu.component and config template and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GameComponent
+    GameComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
